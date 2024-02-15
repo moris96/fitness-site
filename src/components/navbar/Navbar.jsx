0593e2e6a4c4ac657c6a89ce857688a1f5f55e87
moris96 <moriskhoudari@gmail.com>
2024-02-15 10:31:34 -0500

    added logo and custom font to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,61 @@
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+// import { NavLink } from 'react-router-dom';
+
+// import './Navbar.css';
+// import logo from '../../assets/logo.png';
+
+// const Navbar = () => {
+//   return (
+//     <NavbarBs sticky='top' className='navbar navbar-custom navbar-dark'>
+//         <Container>
+//           {/* title */}
+//             <h1 className='h1color'>Spartan Fitness</h1>
+
+//             {/* logo image */}
+//             <img src={logo} alt='logo' className='logo' />
+
+//             {/* navigation */}
+//             <Nav className='ms-auto'>
+//                 <Nav.Link as={NavLink} to='/' exact>Home</Nav.Link>
+//                 <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
+//             </Nav>
+//         </Container>
+//     </NavbarBs>
+//   );
+// };
+
+// export default Navbar;
+
+
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
 
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+
 const Navbar = () => {
   return (
-    <NavbarBs sticky='top' className='navbar navbar-custom navbar-dark bg-dark'>
+    <NavbarBs sticky='top' className='navbar navbar-custom navbar-dark'>
         <Container>
-            <h1>Spartan Fitness</h1>
+          {/* title and logo container */}
+          <div className='title-logo-container'>
+            {/* title */}
+            <h1 className='h1color'>Spartan Fitness</h1>
 
-            {/* navigation */}
-            <Nav className='ms-auto'>
-                <Nav.Link as={NavLink} to='/' exact>Home</Nav.Link>
-                <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
-            </Nav>
+            {/* logo image */}
+            <img src={logo} alt='logo' className='logo' />
+          </div>
+
+          {/* navigation */}
+          <Nav className='ms-auto'>
+              <Nav.Link as={NavLink} to='/' exact>Home</Nav.Link>
+              <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
+          </Nav>
         </Container>
     </NavbarBs>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div id='home'>
       <h1>Home Page</h1>
+      {/* <img src='https://www.worldhistory.org/uploads/images/1225.jpg' alt='home' /> */}
     </div>
   );
 };
