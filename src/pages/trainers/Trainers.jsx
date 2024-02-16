f555f0f7abebacb4874c6f4514af981873ec1a5b
moris96 <moriskhoudari@gmail.com>
2024-02-16 10:37:36 -0500

    updated trainers page

--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -20,9 +20,10 @@ const Trainers = () => {
                   <FontAwesomeIcon icon={faInstagram} />
                   <FontAwesomeIcon icon={faFacebook} />
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh. Morbi in leo in eros commodo volutpat ac sed dolor.</p>
+                <p>The legend himself: King Leonidas of Sparta. He who led 300 men against an army of tens of thousands from Persia. No man is more worthy of training you to be a shield and spear of Sparta.</p>
             </div>
             </div>
+            
             <div className="card d-flex position-relative flex-column">
             <div className='imgContainer'>
                 <img className='kass' src='https://cdn.vox-cdn.com/thumbor/8vaZT34oP9t_etgrtyjbja8tb2o=/0x0:3806x2142/1200x800/filters:focal(1463x45:2071x653)/cdn.vox-cdn.com/uploads/chorus_image/image/61706841/Screen_Shot_2018_09_10_at_12.07.18_PM.0.png' />
@@ -35,9 +36,10 @@ const Trainers = () => {
                   <FontAwesomeIcon icon={faInstagram} />
                   <FontAwesomeIcon icon={faFacebook} />
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh. Morbi in leo in eros commodo volutpat ac sed dolor.</p>
+                <p>Hero of Sparta, Athens, and Atlantis. Descendant and worthy heir of Leonidas. She will help coach you on your journey as a Demigod as well as other valuable skills like stealth, archery, and hunting. She is also our resident Beast Master and will guide you on how to train and befriend the mightiest of creatures.</p>
             </div>
             </div>
+
             <div className="card d-flex position-relative flex-column">
             <div className='imgContainer'>
                 <img className='krat' src='https://oyster.ignimgs.com/mediawiki/apis.ign.com/god-of-war-ragnarok/d/dc/Kratos.jpg' />
@@ -50,9 +52,26 @@ const Trainers = () => {
                   <FontAwesomeIcon icon={faInstagram} />
                   <FontAwesomeIcon icon={faFacebook} />
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh. Morbi in leo in eros commodo volutpat ac sed dolor.</p>
+                <p>He of many names: God of War, Ghost of Sparta, son of Zeus, slayer of Gods and Titans alike. Kratos offers the most intense physical training and will only coach those serious enough about ascending to Godhood.</p>
             </div>
             </div>
+
+            <div className="card d-flex position-relative flex-column">
+            <div className='imgContainer'>
+                <img className='socrates' src='https://pbs.twimg.com/media/Dh29-ZiV4AAptdw.jpg' />
+            </div>
+            <div className="content">
+                <h2>Socrates</h2>
+                {/* Social media icons */}
+                <div className="social-icons">
+                  <FontAwesomeIcon icon={faTwitter} />
+                  <FontAwesomeIcon icon={faInstagram} />
+                  <FontAwesomeIcon icon={faFacebook} />
+                </div>
+                <p>Although born Athenian, Socrates has agreed to teach philosophy and the importance of the arts and sciences to help sharpen warriors' minds so they are sharper in and out of combat. Please note to be careful not to get wrapped up in a philosophical debate lest you wish to debate hours without end.</p>
+            </div>
+            </div>
+
         </div>
     </div>
   );
